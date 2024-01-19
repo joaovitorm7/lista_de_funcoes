909eac7e26952f928f8ddf38e0f5189b0aae31c6
João Vitor Maia Ribeiro <120575645+joaovitorm7@users.noreply.github.com>
2024-01-19 10:55:05 -0300

    Q07

---
+++ quadrado_de_numeros.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+// Função para calcular o quadrado de um número
+float calcularQuadrado(float numero) {
+    return numero * numero;
+}
+
+int main() {
+    char opcao;
+    float numero;
+
+    do {
+        printf("Digite um numero: ");
+
+        if (scanf("%f", &numero) != 1) {
+            while (getchar() != '\n');
+            
+            printf("Entrada invalida. Tente novamente.\n");
+            
+            continue;
+        }
+
+        while (getchar() != '\n');
+
+        float quadrado = calcularQuadrado(numero);
+
+        printf("O quadrado de %.2f eh %.2f\n", numero, quadrado);
+
+        printf("Deseja realizar outro calculo? (S/N): ");
+        scanf(" %c", &opcao);
+
+    } while (opcao == 'S' || opcao == 's');
+
+    printf("Programa encerrado.\n");
+
+    return 0;
+}
