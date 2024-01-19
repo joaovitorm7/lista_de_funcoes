51fca7e8e8bf1b65b23fe3f5b61cf17f56f179af
João Vitor Maia Ribeiro <120575645+joaovitorm7@users.noreply.github.com>
2024-01-19 11:38:46 -0300

    Q01

--- calculando_a_media.c
+++ calculando_a_media.c
@@ -1,29 +1,23 @@
 #include <stdio.h>
 
-// Função para calcular a nota das atividades
 float calcularNotaAtividades(int realizadas, int total) {
-    return ((float)realizadas / total) * 10; // Nota máxima de 10
+    return ((float)realizadas / total) * 10; 
 }
 
-// Função para calcular a média de cada bimestre
 float calcularMediaBimestre(float notaEscrita, float notaPratica, float notaAtividades) {
     return (notaEscrita + notaPratica + notaAtividades) / 3;
 }
 
-// Função para calcular a média final
 float calcularMediaFinal(float mediaBimestre1, float mediaBimestre2, float mediaBimestre3, float mediaBimestre4) {
     return ((mediaBimestre1 + mediaBimestre3) * 3 + (mediaBimestre2 + mediaBimestre4) * 2) / 10;
 }
 
 int main() {
-    // Variáveis para armazenar as notas e informações das atividades de cada bimestre
     float notaEscrita, notaPratica, notaAtividades;
     int totalAtividades, realizadas;
 
-    // Variáveis para armazenar as médias de cada bimestre e a média final
     float mediaBimestre1, mediaBimestre2, mediaBimestre3, mediaBimestre4, mediaFinal;
 
-    // Bimestre 1
     printf("Bimestre 1:\n");
     printf("Nota da avaliacao escrita: ");
     scanf("%f", &notaEscrita);
@@ -37,7 +31,6 @@ int main() {
     notaAtividades = calcularNotaAtividades(realizadas, totalAtividades);
     mediaBimestre1 = calcularMediaBimestre(notaEscrita, notaPratica, notaAtividades);
 
-    // Bimestre 2
     printf("\nBimestre 2:\n");
     printf("Nota da avaliacao escrita: ");
     scanf("%f", &notaEscrita);
@@ -51,7 +44,6 @@ int main() {
     notaAtividades = calcularNotaAtividades(realizadas, totalAtividades);
     mediaBimestre2 = calcularMediaBimestre(notaEscrita, notaPratica, notaAtividades);
 
-    // Bimestre 3
     printf("\nBimestre 3:\n");
     printf("Nota da avaliacao escrita: ");
     scanf("%f", &notaEscrita);
@@ -65,7 +57,7 @@ int main() {
     notaAtividades = calcularNotaAtividades(realizadas, totalAtividades);
     mediaBimestre3 = calcularMediaBimestre(notaEscrita, notaPratica, notaAtividades);
 
-    // Bimestre 4
+
     printf("\nBimestre 4:\n");
     printf("Nota da avaliacao escrita: ");
     scanf("%f", &notaEscrita);
@@ -79,10 +71,8 @@ int main() {
     notaAtividades = calcularNotaAtividades(realizadas, totalAtividades);
     mediaBimestre4 = calcularMediaBimestre(notaEscrita, notaPratica, notaAtividades);
 
-    // Calcular média final
     mediaFinal = calcularMediaFinal(mediaBimestre1, mediaBimestre2, mediaBimestre3, mediaBimestre4);
 
-    // Exibir resultados
     printf("\nMédia Bimestre 1: %.2f\n", mediaBimestre1);
     printf("Média Bimestre 2: %.2f\n", mediaBimestre2);
     printf("Média Bimestre 3: %.2f\n", mediaBimestre3);
